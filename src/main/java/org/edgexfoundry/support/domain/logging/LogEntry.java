02fd1e2bb07fe72ae8f13f6b19ba9442b5dd35b4
Jim White <james_white2@dell.com>
2017-08-14 11:17:57 -0500

    checkstyle changes
    Signed-off-by: Jim White <james_white2@dell.com>

--- src/main/java/org/edgexfoundry/support/domain/logging/LogEntry.java
+++ src/main/java/org/edgexfoundry/support/domain/logging/LogEntry.java
@@ -1,22 +1,21 @@
 /*******************************************************************************
  * Copyright 2016-2017 Dell Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *
- * @microservice:  support-domain
+ * @microservice: support-domain
  * @author: Cloud Tsai and Jude Hung, Dell
  * @version: 1.0.0
  *******************************************************************************/
+
 package org.edgexfoundry.support.domain.logging;
 
 import java.util.Arrays;
@@ -28,73 +27,70 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 public class LogEntry {
 
-	@Id
-	private String id;
-	
-	private Level logLevel;
-	private String[] labels;
-	private String originService;
-	private String message;
-	private long created;
-	
-	public String getId() {
-		return id;
-	}
+  @Id
+  private String id;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+  private Level logLevel;
+  private String[] labels;
+  private String originService;
+  private String message;
+  private long created;
 
-	public Level getLogLevel() {
-		return logLevel;
-	}
+  public String getId() {
+    return id;
+  }
 
-	public void setLogLevel(Level logLevel) {
-		this.logLevel = logLevel;
-	}
+  public void setId(String id) {
+    this.id = id;
+  }
 
-	public String[] getLabels() {
-		return labels;
-	}
+  public Level getLogLevel() {
+    return logLevel;
+  }
 
-	public void setLabels(String[] labels) {
-		this.labels = labels;
-	}
+  public void setLogLevel(Level logLevel) {
+    this.logLevel = logLevel;
+  }
 
-	public String getOriginService() {
-		return originService;
-	}
+  public String[] getLabels() {
+    return labels;
+  }
 
-	public void setOriginService(String originService) {
-		this.originService = originService;
-	}
+  public void setLabels(String[] labels) {
+    this.labels = labels;
+  }
 
-	public String getMessage() {
-		return message;
-	}
+  public String getOriginService() {
+    return originService;
+  }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+  public void setOriginService(String originService) {
+    this.originService = originService;
+  }
 
-	public long getCreated() {
-		return created;
-	}
+  public String getMessage() {
+    return message;
+  }
 
-	public void setCreated(long created) {
-		this.created = created;
-	}
-	
-	public String toString(){
-		StringBuilder builder = new StringBuilder();
-		builder.append(this.created)
-				.append(" [")
-				.append(null==this.originService?"":this.originService)
-				.append("] ")
-				.append(null==this.labels?"[]":Arrays.toString(this.labels))
-				.append(" ")
-				.append(String.format("%1$-5s", this.logLevel.name().toUpperCase())).append(" - ")
-				.append(this.message);
-		return builder.toString();
-	}
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public long getCreated() {
+    return created;
+  }
+
+  public void setCreated(long created) {
+    this.created = created;
+  }
+
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append(this.created).append(" [")
+        .append(null == this.originService ? "" : this.originService).append("] ")
+        .append(null == this.labels ? "[]" : Arrays.toString(this.labels)).append(" ")
+        .append(String.format("%1$-5s", this.logLevel.name().toUpperCase())).append(" - ")
+        .append(this.message);
+    return builder.toString();
+  }
 }
--- src/main/java/org/edgexfoundry/support/domain/notifications/Transmission.java
+++ src/main/java/org/edgexfoundry/support/domain/notifications/Transmission.java
@@ -1,22 +1,21 @@
 /*******************************************************************************
  * Copyright 2016-2017 Dell Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *
- * @microservice:  support-domain
+ * @microservice: support-domain
  * @author: Cloud Tsai and Jude Hung, Dell
  * @version: 1.0.0
  *******************************************************************************/
+
 package org.edgexfoundry.support.domain.notifications;
 
 import java.util.Arrays;
@@ -30,101 +29,101 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "transmission")
 public class Transmission {
 
-	@Id
-	private String id;
-	@DBRef
-	private Notification notification;
-	private String receiver;
-	private Channel channel;
-	private TransmissionStatus status;
-	private int resendCount;
-	private TransmissionRecord[] records;
-	@CreatedDate
-	private long created;
-	@LastModifiedDate
-	private long modified;
+  @Id
+  private String id;
+  @DBRef
+  private Notification notification;
+  private String receiver;
+  private Channel channel;
+  private TransmissionStatus status;
+  private int resendCount;
+  private TransmissionRecord[] records;
+  @CreatedDate
+  private long created;
+  @LastModifiedDate
+  private long modified;
 
-	public String getId() {
-		return id;
-	}
+  public String getId() {
+    return id;
+  }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+  public void setId(String id) {
+    this.id = id;
+  }
 
-	public Notification getNotification() {
-		return notification;
-	}
+  public Notification getNotification() {
+    return notification;
+  }
 
-	public void setNotification(Notification notification) {
-		this.notification = notification;
-	}
+  public void setNotification(Notification notification) {
+    this.notification = notification;
+  }
 
-	public String getReceiver() {
-		return receiver;
-	}
+  public String getReceiver() {
+    return receiver;
+  }
 
-	public void setReceiver(String receiver) {
-		this.receiver = receiver;
-	}
+  public void setReceiver(String receiver) {
+    this.receiver = receiver;
+  }
 
-	public Channel getChannel() {
-		return channel;
-	}
+  public Channel getChannel() {
+    return channel;
+  }
 
-	public void setChannel(Channel channel) {
-		this.channel = channel;
-	}
+  public void setChannel(Channel channel) {
+    this.channel = channel;
+  }
 
-	public TransmissionStatus getStatus() {
-		return status;
-	}
+  public TransmissionStatus getStatus() {
+    return status;
+  }
 
-	public void setStatus(TransmissionStatus status) {
-		this.status = status;
-	}
+  public void setStatus(TransmissionStatus status) {
+    this.status = status;
+  }
 
-	public int getResendCount() {
-		return resendCount;
-	}
+  public int getResendCount() {
+    return resendCount;
+  }
 
-	public void setResendCount(int resendCount) {
-		this.resendCount = resendCount;
-	}
+  public void setResendCount(int resendCount) {
+    this.resendCount = resendCount;
+  }
 
-	public void inreaseResendCount() {
-		this.resendCount++;
-	}
+  public void inreaseResendCount() {
+    this.resendCount++;
+  }
 
-	public TransmissionRecord[] getRecords() {
-		return records;
-	}
+  public TransmissionRecord[] getRecords() {
+    return records;
+  }
 
-	public void setRecords(TransmissionRecord[] records) {
-		this.records = records;
-	}
+  public void setRecords(TransmissionRecord[] records) {
+    this.records = records;
+  }
 
-	public long getCreated() {
-		return created;
-	}
+  public long getCreated() {
+    return created;
+  }
 
-	public void setCreated(long created) {
-		this.created = created;
-	}
+  public void setCreated(long created) {
+    this.created = created;
+  }
 
-	public long getModified() {
-		return modified;
-	}
+  public long getModified() {
+    return modified;
+  }
 
-	public void setModified(long modified) {
-		this.modified = modified;
-	}
+  public void setModified(long modified) {
+    this.modified = modified;
+  }
 
-	@Override
-	public String toString() {
-		return "Transmission [notificationSlug=" + notification != null ? notification.getSlug()
-				: "null" + ", receiver=" + receiver + ", channel=" + channel + ", status=" + status + ", resendCount="
-						+ resendCount + ", records=" + Arrays.toString(records) + "]";
-	}
+  @Override
+  public String toString() {
+    return "Transmission [notificationSlug=" + notification != null ? notification.getSlug()
+        : "null" + ", receiver=" + receiver + ", channel=" + channel + ", status=" + status
+            + ", resendCount=" + resendCount + ", records=" + Arrays.toString(records) + "]";
+  }
 
 }
